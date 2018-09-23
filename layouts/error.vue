1c2f38ab0692bcc8de8cb34b98285427ef70dbce
edo1z <endo.yuta@logicky.com>
2018-09-24 00:54:25 +0900

    Error画面作成

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">404 Not Found</h1>
+    <h1 v-else>Error!</h1>
+    <nuxt-link to="/">ホーム</nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  props: ['error'],
+  layout: 'error_layout'
+}
+</script>
---
+++ layouts/error_layout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <header>
+      <p class="brand">
+        <router-link to="/">Nuxt.js Study</router-link>
+      </p>
+    </header>
+    <div class="container">
+      <nuxt/>
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  padding : 0;
+  margin  : 0;
+}
+body {
+  background : black;
+  color      : white;
+  padding    : 0px;
+  margin     : 0;
+}
+a {
+  color : #f9c;
+}
+a:hover {
+  color : #fbe;
+}
+header {
+  background  : #f9c;
+  color       : black;
+  padding     : 1px 10px;
+  margin      : 0;
+  font-weight : bold;
+}
+header p.brand a, header p.brand a:hover {
+  color           : black;
+  text-decoration : none;
+}
+div.container {
+  padding : 10px 20px;
+}
+</style>
+
