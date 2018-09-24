27fb32373c7f7fb53cc7cc9f064efeb4cd0566f9
edo1z <endo.yuta@logicky.com>
2018-09-24 10:19:17 +0900

    asyncDataを使ってみる

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,44 @@
 <template>
   <section class="container">
     <h1>About</h1>
-    Nice to meet you.
     <router-link to="/">TOP</router-link>
+    <p>Nice to meet you.</p>
+    <table>
+      <tbody>
+      <tr>
+        <th>Name</th>
+        <td>{{ name }}</td>
+      </tr>
+      <tr>
+        <th>Age</th>
+        <td>{{ age }}</td>
+      </tr>
+      <tr>
+        <th>Address</th>
+        <td>{{ address }}</td>
+      </tr>
+      <tr>
+        <th>Job</th>
+        <td>{{ job }}</td>
+      </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 
 <script>
 export default {
-  asyncData (context) {
-
+  //Promiseを返す
+  asyncData ({params}) {
+    return {job: 'Engineer'}
+  },
+  data: function () {
+    return {
+      name: 'Taro',
+      age: 30,
+      address: 'Japan',
+      job: 'Sales'
+    }
   },
   head () {
     return {
@@ -18,4 +47,18 @@ export default {
   }
 }
 </script>
+
+<style>
+table {
+  margin-top : 20px;
+}
+th {
+  background: #fc0;
+  padding:5px;
+  color: black;
+}
+td {
+  padding: 5px;
+}
+</style>
 
